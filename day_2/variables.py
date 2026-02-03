78d34aa94790d7027641136bb45f497c73cd295c
sned-tech <sindouboss@gmail.com>
2026-02-03 21:43:12 +0100

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -16,3 +16,24 @@ first_name, last_name, full_name, country, city, age, year, is_married, is_true,
 type(first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light)
 len(first_name)
 len(last_name)
+
+print(len(first_name > last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_ two / num_one
+remainder = num_two % num_one
+exp = num_one^num_two
+floor_division = num_one//num_two
+
+radius = 30
+area_of_circle = 3.14 * radius * radius
+circum_of_circle = 2 * 3.14 * radius
+area_of_circle = input(radius)
+print(area_of_circle)
+
+first_name, last_name, age = input('Sindou', 'Meite', '15'
+help('keywords')
