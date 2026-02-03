d47727c13dd6ccad9d761362253a9db0f85198bf
sned-tech <sindouboss@gmail.com>
2026-02-03 19:44:57 +0100

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,18 @@
+#day 2:30 Days of python programming
+first_name = 'Sindou'
+last_name = 'Meite'
+full_name = 'Sindou Meite'
+country = 'France'
+city = 'Rosny sous bois'
+age = '15'
+year = '2026'
+is_married = 'False'
+is_true = 'no'
+is_light = 'yes'
 
+first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light = 'Sindou','Meite', 'Sindou Meite', 
+'France', 'Rosny sous bois', '15', '2026', 'False', 'no', 'yes'
+
+type(first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light)
+len(first_name)
+len(last_name)
